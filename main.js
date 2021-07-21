46d695728fcef3c3a635a365eb21d6b493a26b16
arifmp40 <arifmp40@gmail.com>
2021-07-22 05:46:57 +0600

    problem fixed

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
     $(".wrapper i").css("transform", "translate(3rem, -50%)");
     $(".content").addClass("change");
     $("h2").text("Subscribed");
-    $(".content::before").css("bottom", "100%");
     $(".wrapper i").click(function () {
       $(this).removeClass("far fa-bell");
       $(this).addClass("fas fa-bell");
